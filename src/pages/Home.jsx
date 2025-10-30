f4bd2b2bc79d25387bb9bdda86846060f591f526
Matteo Granchelli <yozza@IPHONE.station>
2025-10-30 22:48:47 +0100

    added 2 arrows

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,12 @@ let currentFilter = type ? Data.filter(e => e.Type === type) : Data;
 
 const favTitles = currentFilter.filter(e => e.Favorite==true)
 const currentTitles = currentFilter.filter(e=> e.Current==true)
+const bestToScroll = React.useRef(null)
+function scrollHandler(){
+  if (bestToScroll.current) {
+      bestToScroll.current.scrollBy({ left: 200, behavior: "smooth" });
+    }
+}
 
 return (
     <main>
@@ -25,11 +31,18 @@ return (
         </div>
         <div className="bestContainer">
             <h2>Matteo Favorite's</h2>
-            <div>
-                <ul className="titlesContainer">
+            <div >
+                <div className="arrowLeftContainer">
+                    <span className="arrowLeft"></span>
+                </div>
+                <ul className="titlesContainer" ref={bestToScroll}>
                     <Title Data={favTitles}/>
                 </ul>
+                <div className="arrowRightContainer">
+                    <span className="arrowRight" onClick={scrollHandler}></span>
+                </div>
             </div>
+            {/* create Arrow component, ul scrollable*/}
         </div>
         <div className="currentContainer">
             <h2>Matteo Current's watching</h2>
