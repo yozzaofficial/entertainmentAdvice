fa47ab864bc5b1caf7eb5bd211f5c07b61d21c48
Matteo Granchelli <yozza@Mac.station>
2025-11-04 11:42:35 +0100

    Towatch upgraded

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,20 +1,29 @@
 import React from "react"
 import { nanoid } from 'nanoid';
-export default function Title({Data,openTitleHandler,like}){
+import { MyContext } from "../App";
 
+export default function Title({Data,openTitleHandler,like,likeHandlerToWatch}){
+
+     const {setTitleLiked} = React.useContext(MyContext)
     
+     function removeLike(titleId){
+        setTitleLiked(prev => {return prev.filter(id => id !== titleId)})
+    }     
+
       const liElement = Data.map((e)=>{
         const keyID = nanoid();
         return(
         <li 
             key={keyID} 
-            className="titles" 
+            className={`titles ${likeHandlerToWatch ? "filterToWatch" : ""}`}
             {...(openTitleHandler ? { onClick: () => openTitleHandler(e.id) } : {})}
             {...(like ? { onDoubleClick: () => like(e.id) } : {})}
             style={{
                     backgroundImage:`url("${e.Image}")`,
                     backgroundPosition:"center",
                     backgroundSize:"cover"}}>
+            
+            {likeHandlerToWatch && <span className="likeButtonToWatch" onClick={()=>removeLike(e.id)}></span>}
             </li>)
     })
     
--- src/pages/ToWatch.jsx
+++ src/pages/ToWatch.jsx
@@ -3,22 +3,42 @@ import { useSearchParams } from "react-router-dom";
 import { MyContext } from "../App";
 import Data from "./../titles"
 import Title from ".//../components/Title"
+import addLikeIcon from "./../assets/addLikes.png"
+import { useClickAway } from "react-use";
 import "./css/ToWatch.css"
 export default function ToWatch(){
 
     const {titleLiked} = React.useContext(MyContext)
     const filteredData = Data.filter(e => titleLiked.includes(e.id))
+   
+    const [likeHandlerToWatch, setLikeHandlerToWatch] = React.useState(false)
+    const listToWatch = React.useRef(null)
 
     const [searchParams] = useSearchParams();
     const type = searchParams.get("type");
 
     let currentFilter = type ? filteredData.filter(e => e.Type === type) : filteredData;
+
+    useClickAway(listToWatch, () => {setLikeHandlerToWatch(false)});
+
+    function doubleClickHandler(titleId){
+        setLikeHandlerToWatch(true)
+    }
+ 
     return(
         <div className="likedTitledContainer">
             <h2>Titles To Watch</h2>
-            <ul className="titleLikedList">
-                <Title Data={currentFilter}/>
-            </ul>
+            {currentFilter.length ? 
+                <ul className="titleLikedList" ref={listToWatch}>
+                <Title 
+                    Data={currentFilter}
+                    like={doubleClickHandler}
+                    likeHandlerToWatch={likeHandlerToWatch}/>
+                </ul> : 
+                <div className="emptyLikeContainer">
+                    <h2>Add titles...</h2>
+                    <img src={addLikeIcon} alt="Add like img" />
+                </div>}
         </div>
     )
 }
